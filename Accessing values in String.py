87c18c64c6b82ba77c145810745da6c59616b737
harichandraprasadn <harichandraprasad@globant.com>
2018-12-26 14:44:48 +0530

    Create Accessing values in String.py

---
+++ Accessing values in String.py
@@ -0,0 +1,11 @@
+# Accessing Values in Strings
+
+var1 = "Hari09"
+var2 = "Software Testing"
+
+print("var1[0]:",var1[0])
+print("var2[1:5]",var2[1:5])
+
+x = "Python"
+print(x[:4])
+print(x[:4] + "Hari09")
